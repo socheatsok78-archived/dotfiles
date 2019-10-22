6a2aa3769734d3c69b146036541297b6b6f005ff
Socheat Sok <socheatsok78@gmail.com>
2019-10-22 09:49:19 +0700

    Create ZSH Installer script

---
+++ scripts/zsh.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ./scripts/helper
+
+function check_zsh_installation () {
+    if [ `command -v zsh` ]; then
+        c.success "ZSH is already installed!"
+    else
+        case "$(get_os)" in
+            "Darwin")
+                zsh_install_osx
+            ;;
+            "Linux")
+                zsh_install_linux
+            ;;
+            *)
+                # Drawin
+                zsh_install_osx
+            ;;
+        esac
+    fi
+}
+
+function zsh_install_osx () {
+    c.log "Installing ZSH..."
+    brew install zsh zsh-completions
+    check_zsh_installation
+}
+
+function zsh_install_linux () {
+    c.log "Installing ZSH Linux..."
+    sudo apt-get install zsh
+    check_zsh_installation
+}
+
+function set_default_shell () {
+    c.log "Set ZSH as Default Shell..."
+    sudo chsh -s $(which zsh)
+}
+
+
+function main () {
+    check_zsh_installation
+    set_default_shell
+}
+
+# Entrypoint
+main;
