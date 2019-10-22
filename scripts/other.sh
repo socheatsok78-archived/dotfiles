6546bb85c4fe975f5178938f4d670f8e70516233
Socheat Sok <socheatsok78@gmail.com>
2019-10-22 10:44:33 +0700

    Update Other script

--- scripts/other.sh
+++ scripts/other.sh
@@ -10,11 +10,12 @@ function install_cheat_sh () {
 
 # Install GitIgnore Command Line
 function install_gitignore () {
-    git config --global alias.ignore \
-        '!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi'
+    git config --global alias.ignore '!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi'
 }
 
 function main () {
+    make_local_bin
+
     install_gitignore
     install_cheat_sh
 }
