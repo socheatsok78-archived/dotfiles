9737d5cc2d3415ff5943d3d52d6f274fc4b3aadf
Socheat Sok <socheatsok78@gmail.com>
2019-10-22 12:43:51 +0700

    Fix Other install script

--- scripts/other.sh
+++ scripts/other.sh
@@ -6,12 +6,14 @@ source ./scripts/helper
 
 # Install cheat.sh CLI tool
 function install_cheat_sh () {
-    curl https://cht.sh/:cht.sh | sudo tee "$LOCAL_BIN/cht.sh" && \
+    c.info "Installing cht.sh..."
+    curl -s -o "$LOCAL_BIN/cht.sh" https://cht.sh/:cht.sh && \
         chmod +x "$LOCAL_BIN/cht.sh"
 }
 
 # Install GitIgnore Command Line
 function install_gitignore () {
+    c.info "Installing git ignore alias..."
     git config --global alias.ignore '!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi'
 }
 
