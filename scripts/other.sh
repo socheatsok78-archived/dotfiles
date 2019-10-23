edf45791c122232403acfdc190b5a66a004f2177
Socheat Sok <socheatsok78@gmail.com>
2019-10-23 12:09:50 +0700

    Update Third-party installer script

--- scripts/other.sh
+++ scripts/other.sh
@@ -6,22 +6,25 @@ source ./scripts/helper
 
 # Install cheat.sh CLI tool
 function install_cheat_sh () {
-    c.info "Installing cht.sh..."
+    c.log "Installing cht.sh..."
     curl -s -o "$LOCAL_BIN/cht.sh" https://cht.sh/:cht.sh && \
         chmod +x "$LOCAL_BIN/cht.sh"
 }
 
 # Install GitIgnore Command Line
 function install_gitignore () {
-    c.info "Installing git ignore alias..."
+    c.log "Installing git ignore alias..."
     git config --global alias.ignore '!gi() { curl -sL https://www.gitignore.io/api/$@ ;}; gi'
 }
 
 function main () {
+    c.info "Installing Third-party libraries & tools..."
     make_local_bin
 
     install_gitignore
     install_cheat_sh
+
+    c.success "Third-party libraries & tools successfully installed!"
 }
 
 # Entrypoint
