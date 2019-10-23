f3e6073cce27c043d3188fd18393a0e66816287e
Socheat Sok <socheatsok78@gmail.com>
2019-10-23 19:31:46 +0700

    Create scripts\npm.sh, install http-server and ndb

---
+++ scripts/npm.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+npm install -g http-server  # serve all everything
+npm install -g ndb          # improved debugging experience for Node.js
+# npm install -g tiny-care-terminal    # A little dashboard that tries to take care of you when you're using your terminal.
