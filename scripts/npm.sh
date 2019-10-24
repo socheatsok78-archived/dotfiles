12f7dcbe17172cc7431d471278093a5f015f10dc
Socheat Sok <socheatsok78@gmail.com>
2019-10-24 15:10:49 +0700

    Update scripts\npm.sh, add pretty log

--- scripts/npm.sh
+++ scripts/npm.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
-npm install -g http-server  # serve all everything
-npm install -g ndb          # improved debugging experience for Node.js
-# npm install -g tiny-care-terminal    # A little dashboard that tries to take care of you when you're using your terminal.
+source ./scripts/helper
+
+function install_npm_packages () {
+    npm install -g http-server  # serve all everything
+    npm install -g ndb          # improved debugging experience for Node.js
+    # npm install -g tiny-care-terminal    # A little dashboard that tries to take care of you when you're using your terminal.
+}
+
+function main () {
+    if [ `command -v npm` ]; then
+        c.info "Installing NPM Packages..."
+        install_npm_packages
+        c.success "NPM Packages is installed!"
+    else
+        c.error "[SKIP]: NPM not found!"
+    fi
+}
+
+# Entrypoint
+main;
