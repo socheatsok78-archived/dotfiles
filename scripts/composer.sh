8c12e466d69cca52184fde632ef562485955f458
Socheat Sok <socheatsok78@gmail.com>
2019-11-20 16:08:36 +0700

    Update scripts\composer.sh, add "laravel-zero/installer"

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -16,6 +16,9 @@ function main () {
         composer_install_global "laravel/installer"
         composer_install_global "laravel/valet"
         composer_install_global "laravel/envoy"
+
+        composer_install_global "laravel-zero/installer"
+
         composer_install_global "squizlabs/php_codesniffer"
 
         c.success "Composer Packages is installed!"
