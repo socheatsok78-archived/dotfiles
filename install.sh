429af2749f04e12af810e17085cec7c3f10db9fa
Socheat Sok <alex@socheat.net>
2021-01-10 22:59:36 +0700

    feat: rework "install.sh" and "unattended-install.sh" scripts

--- install.sh
+++ install.sh
@@ -1,19 +1,71 @@
 #!/usr/bin/env bash
 
-source "libs/shared/banner.env"
-source "libs/shared/homebrew.env"
-
-#-------------------------------------------------- traps
 #-------------------------------------------------- pre-flight
-#----- Dotfiles Submodules
-if [ -f ".gitmodules" ]; then
-    ohai "Fetching dotfiles submodules..."
-    git submodule init \
-    && git submodule update
+# string formatters
+if [[ -t 1 ]]; then
+    tty_escape() { printf "\033[%sm" "$1"; }
+else
+    tty_escape() { :; }
 fi
-# -------------------------------------------------- setup
-#----- Install ZSH's profile
-execute "./libs/scripts/install-profile.sh"
 
-#----- Installation successful
-ohai "Installation successful!"
+tty_mkbold() { tty_escape "1;$1"; }
+tty_underline="$(tty_escape "4;39")"
+tty_blue="$(tty_mkbold 34)"
+tty_red="$(tty_mkbold 31)"
+tty_yellow="$(tty_mkbold 33)"
+tty_bold="$(tty_mkbold 39)"
+tty_reset="$(tty_escape 0)"
+
+shell_join() {
+    local arg
+    printf "%s" "$1"
+    shift
+    for arg in "$@"; do
+        printf " "
+        printf "%s" "${arg// /\ }"
+    done
+}
+
+chomp() {
+    printf "%s" "${1/"$'\n'"/}"
+}
+
+ohai() {
+    printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
+}
+
+warn() {
+    printf "${tty_yellow}Warning${tty_reset}: %s\n" "$(chomp "$1")"
+}
+
+error(){
+    printf "${tty_red}Error${tty_reset}: %s\n" "$(chomp "$1")" >>/dev/stderr
+}
+
+abort() {
+  printf "%s\n" "$1"
+  exit 1
+}
+
+execute() {
+  if ! "$@"; then
+    abort "$(printf "Failed during: %s" "$(shell_join "$@")")"
+  fi
+}
+
+# Custom Functions
+brewbundle () {
+    if [ -n "$CI" ]; then
+        execute "brew" "bundle" "--verbose" "--no-lock" "--file" $@
+    else
+        execute "brew" "bundle" "--no-lock"  "--file" $@
+    fi
+}
+
+# -------------------------------------------------- setup
+echo -e "       __      __  _____ __         "
+echo -e "  ____/ /___  / /_/ __(_) /__  _____"
+echo -e " / __  / __ \/ __/ /_/ / / _ \/ ___/"
+echo -e "/ /_/ / /_/ / /_/ __/ / /  __(__  ) "
+echo -e "\__,_/\____/\__/_/ /_/_/\___/____/  "
+echo -e "                                    "
--- unattended-install.sh
+++ unattended-install.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-echo "Unattended Installer"
+source "libs/shared/banner.env"
+source "libs/shared/homebrew.env"
+
+#-------------------------------------------------- traps
+#-------------------------------------------------- pre-flight
+#----- Dotfiles Submodules
+if [ -f ".gitmodules" ]; then
+    ohai "Fetching dotfiles submodules..."
+    git submodule init \
+    && git submodule update
+fi
+# -------------------------------------------------- setup
+#----- Install ZSH's profile
+execute "./libs/scripts/install-profile.sh"
+
+#----- Installation successful
+ohai "Installation successful!"
